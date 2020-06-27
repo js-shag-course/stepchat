1a8b2111410fc6f9b7caccff185f98a24853e29e
Heigen007 <grisakovr@gmail.com>
2020-06-27 08:53:33 +0600

    Предварительный макет header
    Не до конца доделал, есть проблема с добавлением svg, но времени не будет, оставляю так, чтобы для тех, кто будет делать остальные элементы, было от чего отталкиваться в виде позиционирования

--- web/build/assets/script.js
+++ web/build/assets/script.js
@@ -1,40 +1,56 @@
 'use strict'
 
-Vue.component('new-component', {
-   data: {},
-   computed: {},
-   template: `
-    <div>
-        Sub Component: <slot />
-    </div>
-   `,
-   mathods: {
+Vue.component('header-component', {
+    data: {},
+    computed: {},
+    template: `
+     <div>
+        <div class = "logo"> Логотип </div>
+        <div class = "something">Какой-то текст </div>
+        <div class = "user-corner">
+            <div class = "user-photo"> Фото </div>
+            <div class = "username">Имя</div>
+            <input class = "exit-submit" type = "submit" value="Выход"></div>
+        </div>   
+     </div>
+    `,
+    methods: {
+ 
+    } 
+ })
 
-   } 
-})
 
 const app = new Vue({
     el: '#app',
     data: {
-        hello: 'Hello',
-        name: 'ChatiK',
-        field: 'HI HI hI'
+        // hello: 'Hello',
+        // name: 'ChatiK',
+        // field: 'HI HI hI'
     },
     computed: {
-        hiString () {
-            return ` ${ this.hello }, ${ this.name }`
-        }
+        // hiString () {
+        //     return ` ${ this.hello }, ${ this.name }`
+        // }
     },
     template: `
         <div>
-            <h1>{{ field }}</h1>
-            <b>{{ hiString }}</b><br />
-            <input type="text" name="inp" v-model="field" />
-            <new-component class="comp">
-                <b>Slot 1</b>
-            </new-component>
-            <new-component class="comp 2" />
-            <new-component class="comp3" />
+
+            <header-component class = "header" />
+
+
+            <footer-component class = "footer" />
+
         </div>
     `
 })
+
+//мб кому-нибудь этот код пригодится, он из template:
+
+{/* <h1>{{ field }}</h1>
+<b>{{ hiString }}</b><br />
+<input type="text" name="inp" v-model="field" />
+<new-component class="comp">
+    <b>Slot 1</b>
+</new-component>
+<new-component class="comp 2" />
+<new-component class="comp3" /> */}
