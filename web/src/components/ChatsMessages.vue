e1f33a9e73879149ecbf1c67c0bdaf8e9648cde5
adialkozha <49197733+adialkozha@users.noreply.github.com>
2020-06-30 15:46:32 +0600

    chatMessages
    remake getMessages on messages in index.html on line 314-315
    and add vue comp in chatsmessages.vue

---
+++ web/src/components/ChatsMessages.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="messages" @click: (getMessagesJson())>
+    <ul>
+      <li class="sent" v-for="message in getAllMessages" :key = "message" >
+        <img src="http://emilcarlsson.se/assets/mikeross.png" alt="" />
+        <p> {{message.message}}</p>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+import axios from 'axios'
+export default {
+  name: 'messages',
+  data () {
+    return {
+      id: 'Mwh8CZC4S',
+      url: {
+        chatUrl: `http://localhost:3000/chats/messages/${this.id}`
+      },
+      messages: []
+    }
+  },
+  methods: {
+    getMessagesJson () {
+      axios.get(this.url.chatUrl).then(response => {
+        this.messages = response.data
+      })
+    }
+  }
+
+}
+</script>
+<style></style>
