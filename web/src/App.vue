42749445d6388499352d2a6267b9e4ccbc313b12
Giranda22-Git <aloxaeee@bk.ru>
2020-06-30 17:58:51 +0600

    Send Message component

--- web/src/App.vue
+++ web/src/App.vue
@@ -18,6 +18,10 @@ export default {
 </script>
 
 <style lang="scss">
+html, body, form{
+  margin: 0;
+  padding: 0
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
