89cd5b746e205aca617581c500dea94d847bcff9
adialkozha <49197733+adialkozha@users.noreply.github.com>
2020-07-01 14:37:26 +0600

    refactoring

--- server/validation.js
+++ server/validation.js
@@ -1,6 +1,5 @@
-module.exports.checkLength = function(name){
-    if(name.length<32 && name.length>3){
-        return true;
-    }
-    else return false;
-};
+module.exports.checkLength = function (name) {
+  if (name.length < 32 && name.length > 3) {
+    return true
+  } else return false
+}
